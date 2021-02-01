072cddca009f5e7f5c348db25d7b335b2ffdc565
Lin Xiao Zheng <43737599+zhenglinxiao@users.noreply.github.com>
2021-02-01 09:21:42 -0500

    Update __init__.py

--- webapp/foodjiji/__init__.py
+++ webapp/foodjiji/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:onion@localhost/foodjiji'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:-----@localhost/foodjiji'
 db = SQLAlchemy(app)
 
 from foodjiji import routes
